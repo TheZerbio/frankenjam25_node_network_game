f1d6b2520d57ef550889ef696615b25d022547e3
Reskuzo <77434622+Reskuzo@users.noreply.github.com>
2025-11-08 10:24:50 +0100

    Added StandardNote and fixed some merge mishabs

---
+++ Assets/Script/StandardNote.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    public class StandardNote : Node
+    {
+        const int CAPACITY = 20; 
+        const float WORK_RADIUS = 20f;
+        const float VISION_RADIUS = 30f;
+        const float CONNECTION_RADIUS = 10f;
+
+        public const int worker_cost = 10;
+
+
+        public StandardNote(Vector2 position)
+            : base(position, CAPACITY, WORK_RADIUS, VISION_RADIUS, CONNECTION_RADIUS)
+        {
+            
+        }
+        
+        
+        
+    }
+}
