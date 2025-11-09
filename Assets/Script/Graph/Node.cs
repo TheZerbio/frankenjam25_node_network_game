4102395150d035f4e482b91726f76d330f47c2cb
Reskuzo <77434622+Reskuzo@users.noreply.github.com>
2025-11-09 17:26:18 +0100

    Deactivated Hugfe Error Box

--- Assets/Script/Graph/Node.cs
+++ Assets/Script/Graph/Node.cs
@@ -118,7 +118,6 @@ public abstract class Node : MonoBehaviour, ISelectable
         {
             if (lemmingCount < (_NodeDuplicationCost+_edgeCost))
             {
-                UniversalPopUps.ShowOkMessage("You don't have enough Nauts!", "Not enough Nauts!", UniversalDialogueInputPopUp.MESSAGE_TYPE.ERROR);
                 Debug.Log("Can't Afford that new node there, Ey?");
                 /// todo send user a message that he hasn't enough Lemmings
                 return;
